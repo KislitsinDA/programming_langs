a822185fcf7718e93cba9d664e1b69187abc0977
KislitsinDA <107344109+KislitsinDA@users.noreply.github.com>
2024-11-12 10:00:48 +0500

    Update task1.c

--- labs/lab2/task1.c
+++ labs/lab2/task1.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  
+  int n = 10;
+  srand(time(NULL));
+  int array[n];
+  for (int i = 0; i < n; i++) {
+    array[i] = rand() % 100;
+  }
+
+  FILE *fp = fopen("nums.txt", "w");
+  if (fp == NULL) {
+    printf("Ошибка открытия файла!\n");
+    return 1;
+  }
+
+  for (int i = n - 1; i >= 0; i--) {
+    fprintf(fp, "%d\n", array[i]);
+  }
+
+  fclose(fp);
+  printf("Числа записаны в файл 'nums.txt' в обратном порядке.\n");
+  return 0;
+}
 
