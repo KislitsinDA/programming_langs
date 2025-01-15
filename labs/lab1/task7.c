eeb7d36930fabb01d30b99889acbbf31ed55cadf
KislitsinDA <107344109+KislitsinDA@users.noreply.github.com>
2025-01-15 20:50:08 +0500

    Update task7.c

--- labs/lab1/task7.c
+++ labs/lab1/task7.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 1024
+int main() {
+    char line[N];
 
+    const char *text = "text.txt";
+    char *str = "привет";
+    printf("Содержимое файла:\n");
+    FILE *file1 = fopen(text, "r");
+    if(file1)
+    {
+        while((fgets(line, sizeof(line), file1))!=NULL)
+        {
+            printf("%s", line);
+        }
+        
+        fclose(file1);
+    } 
+    FILE *file2 = fopen(text, "a");
+    fputs(str,file2);
+    printf("\n");
+    fclose(file2);
+    
+    printf("Содержимое файла после записи:\n");
+    FILE *file3 = fopen(text,"r");
+    if(file3)
+    {
+        while((fgets(line, sizeof(line), file3))!=NULL)
+        {
+            printf("%s", line);
+        }
+        
+        fclose(file3);
+    } 
+    
+    return 0;
+}
