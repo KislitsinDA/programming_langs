bfa04a1b4398df85e5c5d0947f8c43b5b316794b
KislitsinDA <107344109+KislitsinDA@users.noreply.github.com>
2025-01-15 20:57:22 +0500

    Update task8.c

--- labs/lab1/task8.c
+++ labs/lab1/task8.c
@@ -1,1 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 1024
 
+void func(const char *vivodtext, const char *zapis) {
+    
+    char text1[] = "для временного хранения.txt";
+    FILE *file1, *file2;
+    
+    char buffer[N];
+
+    file1 = fopen(vivodtext, "r");
+    
+    file2 = fopen(text1, "w");
+    
+    fputs(zapis, file2);
+
+    while (fgets(buffer, sizeof(buffer), file1) != NULL) {
+        fputs(buffer, file2);
+    }
+
+    
+    fclose(file1);
+    fclose(file2);
+
+    remove(vivodtext);
+    rename(text1, vivodtext);
+}
+
+int main() {
+    const char *vivodtext = "вывод.txt";
+    char line[N];
+    char zapis[N]; 
+    printf("Содержимое текстового файла:\n");
+    const char *text2 = "вывод.txt";
+    FILE *file3 = fopen(text2, "r");
+    if(file3)
+    {
+        while((fgets(line, sizeof(line), file3))!=NULL)
+        {
+            printf("%s", line);
+        }
+        
+        fclose(file3);
+    } 
+
+    printf("\nВведите строку: ");
+    fgets(zapis, sizeof(zapis), stdin);
+    
+    zapis[strcspn(zapis, "\n")] = '\0';
+
+    func(vivodtext, zapis);
+
+    printf("Итоговая запись в файле:\n");
+    FILE *file4 = fopen(text2,"r");
+    if(file4)
+    {
+        while((fgets(line, sizeof(line), file4))!=NULL)
+        {
+            printf("%s", line);
+        }
+        
+        fclose(file4);
+    }
+    return 0;
+}
